6862a41eef27e173e2f711b369d92026f5530f7d
divya <divyareddy1915@gmail.com>
2022-12-07 14:54:58 +0530

    added preset plugin generator

---
+++ apps/new-preset-plugin-e2e/tests/new-preset-plugin.spec.ts
@@ -0,0 +1,64 @@
+import {
+  checkFilesExist,
+  ensureNxProject,
+  readJson,
+  runNxCommandAsync,
+  uniq,
+} from '@nrwl/nx-plugin/testing';
+
+describe('new-preset-plugin e2e', () => {
+  // Setting up individual workspaces per
+  // test can cause e2e runs to take a long time.
+  // For this reason, we recommend each suite only
+  // consumes 1 workspace. The tests should each operate
+  // on a unique project in the workspace, such that they
+  // are not dependant on one another.
+  beforeAll(() => {
+    ensureNxProject(
+      '@integrated-poc1/new-preset-plugin',
+      'dist/libs/new-preset-plugin'
+    );
+  });
+
+  afterAll(() => {
+    // `nx reset` kills the daemon, and performs
+    // some work which can help clean up e2e leftovers
+    runNxCommandAsync('reset');
+  });
+
+  it('should create new-preset-plugin', async () => {
+    const project = uniq('new-preset-plugin');
+    await runNxCommandAsync(
+      `generate @integrated-poc1/new-preset-plugin:new-preset-plugin ${project}`
+    );
+    const result = await runNxCommandAsync(`build ${project}`);
+    expect(result.stdout).toContain('Executor ran');
+  }, 120000);
+
+  describe('--directory', () => {
+    it('should create src in the specified directory', async () => {
+      const project = uniq('new-preset-plugin');
+      await runNxCommandAsync(
+        `generate @integrated-poc1/new-preset-plugin:new-preset-plugin ${project} --directory subdir`
+      );
+      expect(() =>
+        checkFilesExist(`libs/subdir/${project}/src/index.ts`)
+      ).not.toThrow();
+    }, 120000);
+  });
+
+  describe('--tags', () => {
+    it('should add tags to the project', async () => {
+      const projectName = uniq('new-preset-plugin');
+      ensureNxProject(
+        '@integrated-poc1/new-preset-plugin',
+        'dist/libs/new-preset-plugin'
+      );
+      await runNxCommandAsync(
+        `generate @integrated-poc1/new-preset-plugin:new-preset-plugin ${projectName} --tags e2etag,e2ePackage`
+      );
+      const project = readJson(`libs/${projectName}/project.json`);
+      expect(project.tags).toEqual(['e2etag', 'e2ePackage']);
+    }, 120000);
+  });
+});
---
+++ libs/new-preset-plugin/README.md
@@ -0,0 +1,11 @@
+# new-preset-plugin
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build new-preset-plugin` to build the library.
+
+## Running unit tests
+
+Run `nx test new-preset-plugin` to execute the unit tests via [Jest](https://jestjs.io).
